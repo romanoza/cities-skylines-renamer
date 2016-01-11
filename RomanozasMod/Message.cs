83b90d55c2db52f0e04a4d6bfd494d0cb018668d
Roman <kowaliszyn@gazeta.pl>
2016-01-11 11:40:05 +0100

    City name

--- RomanozasMod/Message.cs
+++ RomanozasMod/Message.cs
@@ -8,6 +8,24 @@ namespace RomanozasMod
 {
     public class Message: MessageBase
     {
+        string _text;
+        uint _residentId;
 
+        public Message(string text): base() {
+            _text = text;
+            _residentId = MessageManager.instance.GetRandomResidentID();
+        }
+
+        public override uint GetSenderID() {
+            return _residentId;
+        }
+
+        public override string GetSenderName() {
+            return CitizenManager.instance.GetCitizenName(_residentId);
+        }
+
+        public override string GetText() {
+            return _text;
+        }
     }
 }
